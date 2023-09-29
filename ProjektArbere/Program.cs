b246d6749abfe079e144928a570b80f6fe701b3d
JonathanJohansson24 <Jonathan.nordin24@gmail.com>
2023-09-29 12:20:52 +0200

    Started to file a bit on the login screen

--- ProjektArbere/Program.cs
+++ ProjektArbere/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-           static string[] users = new string[] { "hans", "greta", "görgen", "frans", "lina", "tage" };
+           static string[] users = new string[] { "hans", "greta", "görgen", "frans", "tage"};
            static int[] codes = new int[5] { 1177, 2323, 4455, 9900, 8585 };
 
         static void Main(string[] args)
@@ -38,7 +38,7 @@
 
             Console.WriteLine("Skriv ditt förnamn för att se om du har ett konto här: ");
             string userName = Console.ReadLine().ToLower();
-            bool located = false;
+            bool userExists = false;
             int tries = 0; 
             for (int i = 0; i < users.Length; i++)
             {
@@ -46,14 +46,32 @@
                 if (users[i] == userName)
                 {
                     Console.WriteLine($"Hej och välkommen {users[i]}");
-                    located = true;
+                    userExists = true;
                 }
             }
-
-             if (!located)
+            if (!userExists)
             {
                 Console.WriteLine($"Tyvärr har vi ingen med namnet som {userName} kund här");
             }   
+
+            Console.WriteLine("Nu vill jag att du matar in din tillhörande pinkod till ditt konto, du har 3 försök på dig");
+            
+            while (tries < 3)
+            {
+                int userIndex = Array.IndexOf(users, userName);
+                Console.Write("Skriv in kod: ");
+                int pinCode = Convert.ToInt32(Console.ReadLine());
+                if (userIndex != -1 && pinCode == codes[userIndex])
+                {
+                    Console.WriteLine("Välkommen in, du skrev rätt pinkod");
+                }
+                else
+                {
+                    Console.WriteLine("Fel kod, försök igen:");
+                    tries++;
+                }
+            }
+            Console.WriteLine("Du skrev fel kod tre gånger, nu stängs programmet ner");
 
 
         }
